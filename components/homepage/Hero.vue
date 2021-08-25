881560b1d058294a54df99bdddc207663db2e5d8
Ridho Febriansa <staff@siesta.id>
2021-08-25 22:42:18 +0700

    fix: nav, footer, and hero content

--- components/homepage/Hero.vue
+++ components/homepage/Hero.vue
@@ -42,13 +42,13 @@
             </p>
             <div class="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
               <p class="text-base font-medium text-gray-900">
-                Sign up to get notified when it’s ready.
+                Sign up to get latest news about PyCon ID 2021.
               </p>
               <form action="#" method="POST" class="mt-3 sm:flex">
                 <label for="email" class="sr-only">Email</label>
                 <input id="email" type="text" name="email" class="block w-full py-3 text-base rounded-md placeholder-gray-500 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:flex-1 border-gray-300" placeholder="Enter your email">
                 <button type="submit" class="mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto">
-                  Notify me
+                  Sign up
                 </button>
               </form>
             </div>
@@ -90,8 +90,12 @@
                 <span class="sr-only">Watch our video to learn more</span>
                 <iframe
                   width="100%"
-                  height="340"
-                  src="https://www.youtube.com/embed/1f6S3x1XUho"
+                  height="315"
+                  src="https://www.youtube-nocookie.com/embed/1f6S3x1XUho"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
                 />
               </button>
             </div>
