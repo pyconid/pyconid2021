239bed4362f84633fbb86201593490b1dfec1670
adifdwimaulana <adif.maulana@thebonza.com>
2021-08-28 23:31:31 +0700

    feat: add prospect section

--- components/pricing/Header.vue
+++ components/pricing/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bg-gray-800">
-    <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:flex lg:justify-between">
-      <div class="max-w-xl">
-        <h2 class="text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
+    <div class="mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:flex lg:justify-between">
+      <div class="w-full">
+        <h2 class="text-4xl text-center font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
           Sponsorship Plans
         </h2>
-        <p class="mt-5 text-xl text-gray-400">
+        <p class="mt-5 text-xl text-center text-gray-400">
           Benefit &amp; Pricing can be seen in the table below
         </p>
       </div>
--- pages/sponsorship.vue
+++ pages/sponsorship.vue
@@ -2,20 +2,20 @@
   <div>
     <Header />
     <Table />
-    <!-- <CloudLogo /> -->
+    <Prospect />
   </div>
 </template>
 
 <script>
 import Header from '@/components/pricing/Header.vue'
 import Table from '@/components/pricing/Table.vue'
-// import CloudLogo from '@/components/pricing/CloudLogo.vue'
+import Prospect from '@/components/pricing/Prospect.vue'
 
 export default {
   components: {
     Header,
-    Table
-    // CloudLogo
+    Table,
+    Prospect
   }
 }
 </script>
