12d5153c00492881d2343ba4cc31c25efd3711e7
Ridho Febriansa <staff@siesta.id>
2021-08-28 20:19:24 +0700

    add: under construction illustration component and implement on empty pages

---
+++ components/global/UnderConstruction.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="relative pt-3 pb-8 sm:pb-12 lg:pb-16 bg-white text-white">
+    <div class="mt-8 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 lg:mt-16">
+      <div class="pt-10 flex h-full">
+        <div class="m-auto text-center">
+          <p class="text-2xl tracking-tight font-extrabold sm:text-3xl xl:text-4xl text-gray-500 p-2 mb-4">
+            This page is still under construction
+          </p>
+          <div class="flex justify-center">
+            <img class="w-full lg:w-2/3 h-auto" src="~/assets/image/wip_illustration.svg" alt="wip illustration">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
