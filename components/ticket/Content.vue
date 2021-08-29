17805bfcb2c0605e4b9e34711c3df11bfb74d310
adifdwimaulana <adif.maulana@thebonza.com>
2021-08-29 17:59:44 +0700

    feat: add ticket content

--- components/ticket/Content.vue
+++ components/ticket/Content.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="bg-white overflow-hidden">
     <div class="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-      <div class="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen" />
       <div class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
         <div>
-          <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-            Case Study
-          </h2>
-          <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            Meet Whitney
+          <h3 class="mt-2 text-3xl text-justify font-bold text-gray-500 sm:text-2xl">
+            PyCon Indonesia 2020 tickets are sold in the following types. Tickets are required to participate in the online conference.
           </h3>
         </div>
       </div>
-      <div class="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
+      <div class="mt-4 lg:grid lg:grid-cols-2 lg:gap-8">
         <div class="relative lg:row-start-1 lg:col-start-2">
           <svg
             class="hidden lg:block absolute top-0 right-0 -mt-20 -mr-20"
@@ -46,31 +42,38 @@
           <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
             <figure>
               <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
-                <img class="rounded-lg shadow-lg object-cover object-center" src="https://images.unsplash.com/photo-1546913199-55e06682967e?ixlib=rb-1.2.1&auto=format&fit=crop&crop=focalpoint&fp-x=.735&fp-y=.55&w=1184&h=1376&q=80" alt="Whitney leaning against a railing on a downtown street" width="1184" height="1376">
+                <img class="rounded-lg shadow-lg object-cover object-center" src="~/assets/image/ticket-placeholder.jpg" alt="Ticket Placeholder">
               </div>
             </figure>
           </div>
         </div>
         <div class="mt-8 lg:mt-0">
-          <div class="text-base max-w-prose mx-auto lg:max-w-none">
-            <p class="text-lg text-gray-500">
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.
-            </p>
-          </div>
           <div class="mt-5 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
-            <p>Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.</p>
-            <p>Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum et et et sit. Faucibus sed non gravida lectus dignissim imperdiet a.</p>
-            <p>Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.</p>
-            <ul role="list">
-              <li>Quis elit egestas venenatis mattis dignissim.</li>
-              <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-              <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+            <ul role="list" class="font-semibold">
+              <li>Regular (Rp. 100.000)</li>
+              <li>Merchandise (Rp. 150.000)</li>
+              <li>Patron (Rp. 1.000.000)</li>
             </ul>
-            <p>Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.</p>
-            <h3>How we helped</h3>
-            <p>Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam. Montes, magna cursus nulla feugiat dignissim id lobortis amet. Laoreet sem est phasellus eu proin massa, lectus. Diam rutrum posuere donec ultricies non morbi. Mi a platea auctor mi.</p>
-            <p>Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.</p>
+            <p>For those who buy PyCon ID 2020 Exclusive Merchandise, delivery of merchandise will be sent using a courier with the Cash on Delivery (COD) system. So you have to pay your shipping fee when your merchandise package has come to your address. Our merchandise will be produced at Surabaya City, East Java. You can choose to send your merchandise from <span class="font-bold text-gray-800">Surabaya</span> or one of these cities: <span class="font-bold text-gray-800">Bandung</span>, <span class="font-bold text-gray-800">Yogyakarta</span>, <span class="font-bold text-gray-800">Bogor</span>, and <span class="font-bold text-gray-800">Makassar</span>. Choosing origin cities other than Surabaya will cost more days on the delivery process. For shipping fee please check at <a href="https://www.jet.co.id/rates" target="_blank" class="font-bold text-gray-900">J&amp;T</a> Express, the merchandise package's weight is approximately 1 kilogram. For international participants or overseas shipping addresses, you can still get your merchandise but you have to pay for your shipping first. Please contact pycon@python.or.id to discuss your shipping fee. We will use <a href="https://www.posindonesia.co.id/en/" target="_blank" class="font-bold text-gray-900 no-underline">POS Indonesia</a> for international shipment.</p>
           </div>
+        </div>
+      </div>
+      <div class="w-full my-8 lg:px-0 sm:px-16">
+        <div v-for="ticket in tickets" :key="ticket.type" class="my-4">
+          <h3 class="text-2xl text-gray-500 font-semibold">
+            {{ ticket.type }}
+          </h3>
+          <no-ssr>
+            <img :src="require(`~/assets/image/tickets/${ticket.image}`)" class="my-4">
+          </no-ssr>
+          <p class="text-gray-500">
+            {{ ticket.description }}
+          </p>
+        </div>
+        <div class="flex justify-center">
+          <a href="https://maimilu.com/e/pycon-id-2021" target="_blank" class="mt-3 w-auto px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-info-800 shadow-sm hover:bg-info-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto">
+            Buy Ticket
+          </a>
         </div>
       </div>
     </div>
@@ -78,5 +81,18 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {}
+  },
+  computed: {
+    tickets () {
+      return [
+        { type: 'Regular Ticket', description: 'This ticket includes event access for two days. We will send the link for the event to your registered email address one week before the event.', image: 'Regular.jpg' },
+        { type: 'Merchandise Ticket', description: 'This ticket includes event access for two days and PyCon ID 2020 Exclusive Merchandise. We will send the link for the event to your registered email address one week before the event. We will send your merchandise in about one week before the event and we will inform your AWB via your registered email.', image: 'Merchandise.jpg' },
+        { type: 'Patron Ticket', description: 'Be a Patron by supporting this event. Your name will be listed on Patron Section. This ticket includes event access for two days and PyCon ID 2020 Exclusive Merchandise. We will send the link for the event to your registered email address one week before the event. We will send your merchandise in about one week before the event and we will inform your AWB via your registered email.', image: 'Patron.jpg' }
+      ]
+    }
+  }
+}
 </script>
--- pages/ticket.vue
+++ pages/ticket.vue
@@ -1,12 +1,17 @@
 <template>
-  <Content />
+  <div>
+    <Header />
+    <Content />
+  </div>
 </template>
 
 <script>
+import Header from '@/components/ticket/Header.vue'
 import Content from '@/components/ticket/Content.vue'
 
 export default {
   components: {
+    Header,
     Content
   }
 }
