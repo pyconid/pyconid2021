3f074e109ea483718b9ba65ca50201257b8c893c
Ridho Febriansa <staff@siesta.id>
2021-09-12 14:05:23 +0700

    add: dedicated sponsor page with lorem ipsum content

---
+++ pages/sponsors/lorem-ipsum.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="relative px-4 py-16 bg-white overflow-hidden">
+    <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+      <div class="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+        <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+          <defs>
+            <pattern
+              id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+        </svg>
+        <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+          <defs>
+            <pattern
+              id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+        </svg>
+        <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+          <defs>
+            <pattern
+              id="d3eb07ae-5182-43e6-857d-35c643af9034"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+        </svg>
+      </div>
+    </div>
+    <div class="relative px-4 sm:px-6 lg:px-8">
+      <div class="text-lg max-w-prose mx-auto">
+        <h1 class="mb-8">
+          <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">Introducing</span>
+          <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Lorem Ipsum</span>
+        </h1>
+        <figure class="flex justify-center my-2">
+          <img class="w-auto h-80 rounded-lg" src="https://ik.imagekit.io/9rpt33mvmla/logo/Logo%20Bright-01.png" alt="" width="1310" height="873">
+          <figcaption />
+        </figure>
+        <p class="text-xl text-gray-500 leading-8">
+          Welcome to Lorem Ipsum Virtual Booth at PyCon Indonesia 2021, #Pythonistas! Hope that you all read from 1) Talks from Lorem Ipsum in PyCon ID 2021, to remaining parts below like 2) #Lorem #Ipsum and Python, and 3) More resources. 🐍
+        </p>
+      </div>
+      <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+        <p>Talks from Lorem Ipsum in PyCon ID 2021</p>
+        <ol role="list">
+          <li>"Managing Data in Lorem Ipsum Ecosystem" by <strong>Lorem Ipsum</strong> (Lorem Ipsum MVP)</li>
+          <li>"Cloud Development using Python" by <strong>Lorem Ipsum</strong> (Cloud Architect at Lorem Ipsum)</li>
+          <li>"TinyML with MicroPython" by <strong>Lorem Ipsum</strong> (CEO, Lorem Ipsum; Vice, Lorem Ipsum IoT Association)</li>
+        </ol>
+        <h2>"Managing Data in Lorem Ipsum Ecosystem" by <strong>Lorem Ipsum</strong> (Lorem Ipsum MVP)</h2>
+        <div class="flex flex-col md:flex-row gap-x-4">
+          <figure class="flex justify-center w-full my-2">
+            <img class="w-auto h-60 rounded-lg" src="https://ik.imagekit.io/9rpt33mvmla/logo/Logo%20Bright-01.png" alt="">
+            <figcaption />
+          </figure>
+          <p class="text-justify">
+            On this session, Ridi will tell you a story about how to manage data science project with MLOps, Microsoft Azure, and Visual Studio.<br><br>
+            Ridi is certified trainer for Microsoft +12 years, Microsoft MVP in developer tools for + 14 years, and post-doctorate researcher in software engineering.
+          </p>
+        </div>
+        <h2>"Cloud Development using Python" by <strong>Lorem Ipsum</strong> (Cloud Architect at Lorem Ipsum)</h2>
+        <div class="flex flex-col md:flex-row gap-x-4">
+          <figure class="flex justify-center w-full my-2">
+            <img class="w-auto h-60 rounded-lg" src="https://ik.imagekit.io/9rpt33mvmla/logo/Logo%20Bright-01.png" alt="">
+            <figcaption />
+          </figure>
+          <p class="text-justify">
+            On this session, Jo will talk about "Cloud Native Development for Python" which focus on offline/online development (using WSL, Codespaces) and DevOps (CI/CD and IaC).<br><br>
+            Jo is an experienced data architect; designing an end to end data solution to fulfill business needs.
+          </p>
+        </div>
+        <h2>#Microsoft #Azure and Python</h2>
+        <p class="text-justify">
+          Microsoft and PyCon have had a strong relationship globally for years. This year, especially, there were also online PyCon US on last May, and PyCon APAC 2020 organized by PyCon Malaysia with Microsoft sponsorship (Link 1, Link 2, Link 3). Microsoft has worked with Python communities to create tools and services which provide ease of development.<br><br>
+          Invent with purpose: Empower your inner inventor to turn ideas into outcomes with Microsoft Azure. Python is great to build and deploy your applications, and further to apply empowering AI & data science functionalities. At PyCon US 2020 which happened online in last May, Microsoft shared the improvements we’ve made for Python developers in Azure, especially our newer offerings that enable you to build and deploy Python applications in the cloud at any scale. You can check out the details of Microsoft's presentations at https://www.microsoft.com/en-us/boards/pycon2020.aspx. Also, read about what Microsoft loves about PyCon and the Python community on the "Microsoft’s Python team loves PyCon, including this year’s online version!" article, which is really cool!
+        </p>
+        <h2>More resources</h2>
+        <p class="text-justify">
+          More various content for Python users is available at Microsoft Docs and Learn.
+        </p>
+        <ol role="list">
+          <li>Azure for Python Developers: Deploy your Python code to Azure for web apps, serverless apps, containers, and machine learning models. Take advantage of the Azure libraries (SDK) for Python to programmatically access the full range of Azure services including storage, databases, pre-built AI capabilities, and much more.</li>
+          <li>Build an AI web app by using Python and Flask: Use Python, Flask, and Azure Cognitive Services to build a web app that incorporates AI.</li>
+          <li>Python-Azure-IoT-Central-and-Docker-Container-Debugging tutorial and a sponsor session were done at PyCon Korea 2019 last year, and Python + AI GitHub repo content was presented at PyCon Kiwi last year - those are also great resources with Azure and Python.</li>
+        </ol>
+        <p class="text-justify">
+          Get the latest articles, documentation, and events from Microsoft.Source - the curated monthly developer community newsletter. Learn about new technologies and find opportunities to connect with other developers online and locally. Thank you for coming to Microsoft virtual booth at PyCon Indonesia 2020!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
