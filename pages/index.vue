eea8b8c0934db1e56f44a3bcdfc7d7ef8f8764d9
wildan <wildanrgssport@gmail.com>
2021-10-31 22:46:18 +0700

    add keynote speaker component and show it on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,19 @@
         </div>
       </div>
     </div>
-    <KeynoteSpeaker />
+    <div class="bg-white overflow-hidden">
+      <div class="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lq:px-8">
+        <div class="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen" />
+        <div class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
+          <div>
+            <h3 class="mt-2 mb-4 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              Keynote Speaker
+            </h3>
+          </div>
+        </div>
+        <KeynoteSpeaker shadow="sm" />
+      </div>
+    </div>
     <SNS />
     <div>
       <YoutubePlaylist />
