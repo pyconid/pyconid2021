4d5802ff253bf0893edbf79239e0ccfb787a641b
Ridho Febriansa <staff@siesta.id>
2021-12-02 08:50:09 +0700

    add: redirect to hubilo on /start through default middleware

--- middleware/call_for_paper.js
+++ middleware/call_for_paper.js
@@ -1,5 +1,7 @@
 export default function (context) {
   if (context.route.fullPath === '/call-for-proposal') {
     return context.redirect('/call-for-paper')
+  } else if (context.route.fullPath === '/start') {
+    return context.redirect('https://pyconid2021.hubilo.com/community/#/login')
   }
 }
