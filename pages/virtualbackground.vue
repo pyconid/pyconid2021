913ff402c28c0b3556ea7e88e478c2ce02c1be74
Ridho Febriansa <staff@siesta.id>
2021-12-03 16:23:51 +0700

    add: virtual background redirect link

---
+++ pages/virtualbackground.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.location.href = 'https://drive.google.com/file/d/12wKPdi3WQUpZUeDOKNlGawJh4Qy9awhE/view'
+  }
+}
+</script>
