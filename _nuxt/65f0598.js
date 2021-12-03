5fe94ce59388cdac73fc9215356dc340ce44cee1
ridhof <ridhof@users.noreply.github.com>
2021-12-03 09:26:26 +0000

    deploy: 913ff402c28c0b3556ea7e88e478c2ce02c1be74

---
+++ _nuxt/65f0598.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{282:function(e,n,t){"use strict";t.r(n);var o={mounted:function(){window.location.href="https://drive.google.com/file/d/12wKPdi3WQUpZUeDOKNlGawJh4Qy9awhE/view"}},l=t(14),component=Object(l.a)(o,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
